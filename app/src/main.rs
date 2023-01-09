6fe17e319c83ea028a6a02632e1945a72bf021b4
yoyo-kikuchi <rk2325357reply@yahoo.co.jp>
2023-01-09 20:51:14 +0900

    first api作成

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct ArticleQuery {
+    name: String,
+}
+
+#[get("/greet")]
+async fn greet(query: web::Query<ArticleQuery>) -> impl Responder {
+    HttpResponse::Ok().body(format!("name is {}", query.name))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/sample", web::get().to(|| async { "Hello World!" }))
+            .service(greet)
+    })
+    .bind(("0.0.0.0", 3000))?
+    .run()
+    .await
 }
